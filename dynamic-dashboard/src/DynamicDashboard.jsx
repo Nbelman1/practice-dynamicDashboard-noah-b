2f99058124f6154509710e66a7135e624499233d
Noah <nbelman1@gmail.com>
2025-10-15 22:36:50 -0400

    Completed instructions 1-3

---
+++ dynamic-dashboard/src/DynamicDashboard.jsx
@@ -0,0 +1,43 @@
+const DynamicDashboard = () => {
+
+    // instruction 1. dynamic greeting
+    let userName = "Noah";
+    const renderUserGreeting = `Hello, ${userName}`;
+
+    // instruction 2. conditional content
+    let isPremium = true;
+    const subscriptionMsg = (isPremium ? "Thank you for being a premium member!" : "Upgrade to premium to enjoy exclusive features!");
+
+    // instruction 3. render a task lisk
+    let myTasks = [
+        {"name": "Wash dishes", "isCompleted": true},
+        {"name": "Do laundry", "isCompleted": true},
+        {"name": "Study React", "isCompleted": false}
+    ];
+    const checkEmoji = "\u2705"; // ✅
+    const xEmoji = "\u274c"; // ❌
+    
+    function isCompletedEmoji(task) {
+        if (task.isCompleted) {
+            return checkEmoji;
+        } else if (!task.isCompleted) {
+            return xEmoji;
+        }
+    };
+
+    const renderTaskList = myTasks.map((task) => 
+        <li>{isCompletedEmoji(task)} {task.name}</li>
+    );
+
+    // return statement
+    return (
+        // wrap in div so JSX elements have just one parent
+        <div>
+            <h1>{renderUserGreeting}</h1>
+            <h2>{subscriptionMsg}</h2>
+            <ul>{renderTaskList}</ul>
+        </div>
+    );
+}
+
+export default DynamicDashboard;
